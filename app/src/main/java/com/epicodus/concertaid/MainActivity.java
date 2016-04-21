003c9d4343d6bbe0ea055147d303b4a877c82c49
Kyle <kderrick1988@gmail.com>
2016-04-21 15:41:25 -0700

    Bind views with butterknife on mainActivity

--- app/src/main/java/com/epicodus/concertaid/MainActivity.java
+++ app/src/main/java/com/epicodus/concertaid/MainActivity.java
@@ -2,12 +2,21 @@ package com.epicodus.concertaid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.submitLoginButton) Button mSubmitLoginButton;
+    @Bind(R.id.etUsername) EditText mETUsername;
+    @Bind(R.id.etPassword) EditText mETPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
